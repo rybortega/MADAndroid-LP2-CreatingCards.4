4e925813cb5f161affbf9501ae882314bdcdbd8d
Adewale Oyedele <adeoyedewale1@gmail.com>
2022-02-28 09:30:35 -0600

    additional optional  features added

--- app/src/main/java/com/example/flashcardsapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flashcardsapp;
 
 import static android.graphics.Color.YELLOW;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -9,6 +10,7 @@ import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         TextView tvAnswer1 = findViewById(R.id.tvAnswer1);
         TextView tvAnswer2 = findViewById(R.id.tvAnswer2);
         TextView tvAnswer3 = findViewById(R.id.tvAnswer3);
+        ImageView toggleChoiceVisibility = findViewById(R.id.toggle_choice_visibility);
+        ImageView toggleChoiceInvisibility = findViewById(R.id.toggle_choice_invisibility);
         flashcardQuestion.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -64,6 +68,28 @@ public class MainActivity extends AppCompatActivity {
                 tvAnswer1.setBackgroundColor(Color.parseColor("#f6b26b"));
                 tvAnswer2.setBackgroundColor(Color.parseColor("#f6b26b"));
                 tvAnswer3.setBackgroundColor(Color.parseColor("#f6b26b"));
+            }
+        });
+
+        toggleChoiceVisibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvAnswer1.setVisibility(View.INVISIBLE);
+                tvAnswer2.setVisibility(View.INVISIBLE);
+                tvAnswer3.setVisibility(View.INVISIBLE);
+                toggleChoiceVisibility.setVisibility(View.INVISIBLE);
+                toggleChoiceInvisibility.setVisibility(View.VISIBLE);
+            }
+        });
+
+        toggleChoiceInvisibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvAnswer1.setVisibility(View.VISIBLE);
+                tvAnswer2.setVisibility(View.VISIBLE);
+                tvAnswer3.setVisibility(View.VISIBLE);
+                toggleChoiceVisibility.setVisibility(View.VISIBLE);
+                toggleChoiceInvisibility.setVisibility(View.INVISIBLE);
             }
         });
     }
